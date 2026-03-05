3001f49da4e92fc12670aba2219e4bc47bf70244
弈全 陈 <Davidchenyiquan@hotmail.com>
2026-03-05 23:16:10 +0800

    提交src文件夹下包含一个main.c文件进行测试

---
+++ src/mian.c
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int a = 0x12345678;
+	int b = 0x87654321;
+	int c = a ^ b;
+	printf("a: %08x\n", a);
+	printf("b: %08x\n", b);
+	printf("c: %08x\n", c);
+	return 0;
+}
